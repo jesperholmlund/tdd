91ce237b3499654fd64884fe87d57e63516ae825
jesperholmlund <jesper.holmlund@student.kyh.se>
2022-06-03 18:24:04 +0200

    description

--- fetch.test.js
+++ fetch.test.js
@@ -1,5 +1,9 @@
 const fetch = require("./fetch");
 
+/**
+ * testing fetching by looking first to se if defined, then if response is an object
+ * Since we know how many teams are in NHL, we check if we get 32 teams
+ */
 describe("fetch", () => {
   it("should return list of teams", async () => {
     const teams = await fetch.fetchTeams();
@@ -7,6 +11,9 @@ describe("fetch", () => {
     expect(typeof teams === "object").toBeTruthy();
     expect(teams.length).toBe(32);
   });
+  /**
+   * Checking if the response actually contains NHL teams by looking for a specific team
+   */
   it("should contain Edmonton Oilers", async () => {
     const teams = await fetch.fetchTeams();
     const team = await teams.map((t) => {
@@ -14,6 +21,9 @@ describe("fetch", () => {
     });
     expect(team).toContain("Edmonton Oilers");
   });
+  /**
+   * Check each team to see if the team is actually an active NHL
+   */
   it("should be actives team", async () => {
     const teams = await fetch.fetchTeams();
     await teams.map((t) => {
@@ -21,7 +31,11 @@ describe("fetch", () => {
     });
   });
 });
-
+/**
+ * controlls if a chosen teams properties
+ * first what typeof properties
+ * then if the team belongs to a division
+ */
 describe("single team", () => {
   it("should return", async () => {
     const team = await fetch.fetchTeams();
--- script.test.js
+++ script.test.js
@@ -3,9 +3,11 @@ const scripts = require("./script");
 describe("Mail ", () => {
   const mails = scripts.inbox();
   describe("inbox", () => {
+    //expecting the inbox to have 3 mails
     it("should return three entires", () => {
       expect(mails.length).toBe(3);
     });
+    // loops the inbox and then check for duplicate ID:s
     it("should return unique id for every mail", () => {
       const ids = [];
       mails.map((mail) => {
@@ -14,10 +16,13 @@ describe("Mail ", () => {
       const duplicates = ids.some((item, i) => i !== ids.indexOf(item));
       expect(duplicates).toBeFalsy();
     });
+    //check one mail's length
     it("should be able to search inbox", () => {
       expect(scripts.searchMail("dolor").length).toBe(3);
     });
   });
+  //creates a new mail and check if the mail ID is unique
+  //by comparing it with each mail in the inbox
   describe("new mail", () => {
     it("should have unique id", () => {
       const found = scripts
@@ -25,6 +30,7 @@ describe("Mail ", () => {
         .find((mail) => mail._id == scripts.newMail[0]._id);
       expect(found).toBeUndefined();
     });
+    //check if new mail have not empty field by checking for length of fields
     it("should have all fields not empty", () => {
       scripts.newMail.map((row) => {
         expect(row._id).not.toEqual(0);
